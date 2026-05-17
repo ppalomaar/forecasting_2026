f3f7d279505bc83300dad7ba0a36b37dee205c72
ppalomaar <paloma.ransi@gmail.com>
2026-05-17 20:57:13 +0700

    orde beda

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ data_historis = load_data()
 def train_arimax_model(df):
     endog = df['Harga_NilaiTukar']
     exog = df['Harga']
-    model = sm.tsa.ARIMA(endog, order=(1, 1, 2), exog=exog)  
+    model = sm.tsa.ARIMA(endog, order=(1, 0, 3), exog=exog)  
     model_fitted = model.fit()
     return model_fitted
 
